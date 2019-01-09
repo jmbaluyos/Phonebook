f58f0261ae69c2a20ae862121879ac0e508d2731
John Michael <jmbaluyos@gmail.com>
2019-01-09 15:24:50 +0800

    modify

--- addnew.php
+++ addnew.php
@@ -19,10 +19,11 @@
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
-		  <a class="navbar-brand" href="index.php">Welcome to Phonebook</a>
+		  <a class="navbar-brand" href="#">Welcome to Phonebook</a>
 		  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
 		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 		      <li class="nav-item active">
+		      		<button type="button" class="btn btn-outline-info btn-sm fa fa-home"><a class="navbar-brand" href="index.php?username=<?php echo $_GET['username']; ?>">Home</a></button>
 		      </li>
 		    </ul>
 		    	<div class="btn-group">
